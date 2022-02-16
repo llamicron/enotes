5bcc4094521ca5808d9b0a176471f2088c5b6918
llamicron <llamicron@gmail.com>
2022-02-16 13:17:27 -0600

    Adds basic rocket boilderplate

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Enotes
+This is a simple backend for a notes application, so Eman can write a frontend.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_json;
+
+// The Rocket JSON type, used to wrap types
+use rocket::serde::{json::Json};
+// The serde_json Value type, a generic Json type
+use serde_json::Value;
+
+#[get("/")]
+fn index() -> Value {
+    json!({
+        "msg": "ok!"
+    })
+}
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api/0.1.0", routes![index])
 }
