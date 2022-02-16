9d5340f61ec39d1c47e707df5c6934ba4ae4d304
llamicron <llamicron@gmail.com>
2022-02-16 13:35:01 -0600

    Adds route catcher for not found errors

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,15 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde_json;
 
+
 // The Rocket JSON type, used to wrap types
+#[allow(unused)]
 use rocket::serde::{json::Json};
 // The serde_json Value type, a generic Json type
 use serde_json::Value;
 
+
+// A basic route to test if the API is running. Should return with an Ok message
 #[get("/")]
 fn index() -> Value {
     json!({
@@ -15,8 +19,16 @@ fn index() -> Value {
     })
 }
 
+#[catch(404)]
+fn not_found() -> Value {
+    json!({
+        "status": i32::from(404),
+        "msg": "route not found"
+    })
+}
+
 
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/api/0.1.0", routes![index])
+fn launch() -> _ {
+    rocket::build().register("/api/0.1.0", catchers![not_found]).mount("/api/0.1.0", routes![index])
 }
