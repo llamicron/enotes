789f8c51a0794e986c11761f482929d5fc3c8448
llamicron <llamicron@gmail.com>
2022-02-23 17:49:42 -0600

    Makes a maybe function db??

--- src/db.rs
+++ src/db.rs
@@ -1,37 +1,98 @@
-use std::collections::HashMap;
-use rustbreak::{MemoryDatabase, FileDatabase, deser::Yaml};
-use std::fs::File;
-
-
-use crate::{NoteID, Note};
+use std::{collections::HashMap, fs, io::Write, path::Path};
+use crate::note::{NoteID, Note};
 
 #[derive(Debug)]
 pub struct DBError(String);
 
-pub type Result<T> = std::result::Result<T, DBError>;
+type Result<T> = std::result::Result<T, DBError>;
+type NoteCollection = HashMap<NoteID, Note>;
 
-pub struct Database(pub FileDatabase<HashMap<NoteID, Note>, Yaml>);
+#[derive(Debug)]
+pub struct Database {
+    inner: NoteCollection,
+    file_path: String
+}
 
 impl Database {
     pub fn connect(file_path: &str) -> Result<Database> {
-        // Will create the file if it doesn't already exist
-        let db_file = File::create("./enotes.db").unwrap();
-        let db = FileDatabase::<HashMap<NoteID, Note>, Yaml>::from_file(db_file, HashMap::new());
+        if !Path::new(file_path).exists() {
+            let file = fs::File::create(file_path).map_err(|e| {
+                DBError(format!("Could not create db file: {}", e))
+            })?;
+        }
+
+        let file_contents = fs::read_to_string(file_path).expect("Could not read from db file");
+        let inner = serde_json::from_str::<NoteCollection>(&file_contents).unwrap();
         
-        db
-            .map(|db|  Database(db) )
-            .map_err(|err| {
-                DBError(format!("Could not connect to db: {}", err))
-            })
+        Ok(Database {
+            inner,
+            file_path: String::from(file_path)
+        })
     }
 
-    pub fn immolate(&self) -> Result<()> {
-        self.0.write(|db| {
-            db.clear();
-        }).expect("Couldn't clear database");
-        self.0.save().map(|_| () ).map_err(|err| DBError(format!("Couldn't save database: {}", err)) )
+    fn persist(&mut self) -> Result<()> {
+        let data = serde_json::to_string(&self.inner)
+            .map_err(|e| DBError(format!("{}", e) ))?;
+
+        let mut file = fs::File::options().write(true).open(&self.file_path).unwrap();
+        file.write_all(data.as_bytes()).map_err(|e| {
+            DBError(format!("Could not persist database: {}", e))
+        })
+    }
+
+    fn wipe(&mut self) -> Result<()> {
+        self.inner.clear();
+        self.persist()?;
+        Ok(())
+    }
+
+    pub fn all_notes(&mut self) -> &NoteCollection {
+        &self.inner
+    }
+
+    pub fn get_note(&mut self, id: NoteID) -> Result<&Note> {
+        self.inner.get(&id).ok_or(DBError(format!("Note {} not found", id)))
+    }
+
+    pub fn update_note(&mut self, id: NoteID, mut new_note: Note) -> Result<Note> {
+        if self.inner.get(&id).is_some() {
+            let updated = new_note.clone();
+            self.inner.insert(id, updated);
+            self.persist().ok();
+            Ok(new_note)
+        } else {
+            Err(DBError(format!("Note {} not found", id)))
+        }
+    }
+
+    pub fn create_note(&mut self, note: Note) -> Result<Note> {
+        let keys = &mut self.inner.keys().collect::<Vec<&NoteID>>();
+        let highest = keys.iter().max();
+        let id: NoteID = if highest.is_none() { 0 } else { *highest.unwrap() + 1 };
+
+        let new_note = Note::new(id, note.title(), note.content());
+        let returned = new_note.clone();
+        self.inner.insert(id, new_note);
+
+        self.persist().ok();
+
+        Ok(note)
+    }
+
+    pub fn delete_note(&mut self, id: NoteID) -> Result<()> {
+        let result = match self.inner.remove(&id) {
+            Some(_) => Ok(()),
+            None => Err(DBError(format!("Couldn't delete note")))
+        };
+        self.persist().ok();
+        result
     }
 }
+
+
+
+
+
 
 
 
@@ -39,52 +100,36 @@ impl Database {
 mod tests {
     use super::*;
 
+    fn db() -> Database {
+        Database::connect("./enotes.db").unwrap()
+    }
 
     #[test]
-    fn db_connect() {
+    fn test_db_connection() {
         let db = Database::connect("./enotes.db");
         assert!(db.is_ok());
     }
 
     #[test]
-    fn test_read_write_to_db() {
-        let note = Note::new(54, "note title", "note content");
-
-        let db = Database::connect("./enotes.db").unwrap();
-
-        db.0.write(|db| {
-            db.insert(54, note);
-        }).ok();
-
-        db.0.read(|db| {
-            match db.get(&54) {
-                Some(found_note) => assert_eq!(found_note.title(), "note title"),
-                None => assert!(false),
-            }
-        }).ok();
-
-        db.immolate().unwrap();
+    fn test_persist() {
+        let mut db = db();
+        let note = Note::new(8, "title", "content");
+        db.inner.insert(8, note);
+        db.persist().ok();
     }
 
     #[test]
-    fn test_delete_note() {
-        let note = Note::new(54, "note title", "note content");
-
-        let db = Database::connect("./enotes.db").unwrap();
-
-        db.0.write(|db| {
-            db.insert(54, note);
-        }).ok();
-
-        db.0.write(|db| {
-            db.remove(&54);
-        }).ok();
-
-        db.0.read(|db| {
-            assert!(db.get(&54).is_none());
-        }).ok();
-
-        db.immolate().unwrap();
+    fn test_get_all_notes() {
+        let mut db = db();
+        assert!(db.all_notes().len() > 0);
     }
-    
+
+    #[test]
+    fn test_get_note() {
+        let mut db = db();
+        let note = db.get_note(6);
+        assert!(note.is_ok());
+        let not_found = db.get_note(12313);
+        assert!(not_found.is_err());
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate serde_json;
 
-extern crate rustbreak;
 
 // The Rocket JSON type, used to wrap types
 #[allow(unused)]
@@ -14,18 +13,7 @@ use rocket::State;
 pub mod db;
 pub mod note;
 
-use db::{Database};
 use note::{Note, NoteID};
-
-// This function is garbage
-// I should attach the DB as a State<Database> attachment to
-// rocket but I can't implement FromRequest for the rustbreak DB.
-// I should slog through the lifetime shit or use a real database.
-// I'll just connect to the db for each request. This will *not* work at
-// scale but it'll work for now.
-fn db() -> Database {
-    Database::connect("./enotes.db").expect("Couldn't connect to the DB")
-}
 
 // Basic routes ------
 
@@ -51,95 +39,92 @@ fn not_found() -> serde_json::Value {
 
 // Resource management ------
 
-#[get("/note/<id>")]
-fn get_note(id: NoteID) -> serde_json::Value {
-    // Retrieve the note from the database/storage
-    // Hardcoded note for testing
+// #[get("/note/<id>")]
+// fn get_note(id: NoteID) -> serde_json::Value {
+//     // Retrieve the note from the database/storage
+//     // Hardcoded note for testing
 
-    let db = db();
-    db.0.read(|db| {
-        if let Some(note) = db.get(&id) {
-            return json!({
-                "status": i32::from(200),
-                "msg": "",
-                "data": serde_json::to_string(&note).unwrap()
-            });
-        } else {
-            return json!({
-                "status": i32::from(404),
-                "msg": format!("note not found at id {}", id)
-            });
-        }
-    }).unwrap();
+//     db.0.read(|db| {
+//         if let Some(note) = db.get(&id) {
+//             return json!({
+//                 "status": i32::from(200),
+//                 "msg": "",
+//                 "data": serde_json::to_string(&note).unwrap()
+//             });
+//         } else {
+//             return json!({
+//                 "status": i32::from(404),
+//                 "msg": format!("note not found at id {}", id)
+//             });
+//         }
+//     }).unwrap();
 
-    return json!({
-        "status": i32::from(404),
-        "msg": format!("note not found at id {}", id)
-    });
-}
+//     return json!({
+//         "status": i32::from(404),
+//         "msg": format!("note not found at id {}", id)
+//     });
+// }
 
-#[post("/note", format = "json", data = "<json_note>")]
-fn create_note(json_note: Json<Note>) -> serde_json::Value {
-    let db = db();
+// #[post("/note", format = "json", data = "<json_note>")]
+// fn create_note(json_note: Json<Note>) -> serde_json::Value {
+//     let db = db();
 
-    db.0.write(|db| {
-        let highest = db.iter().max_by(|a, b| a.0.cmp(&b.0)).map(|(k, _v)| k).unwrap();
-        let id = highest + 1;
+//     db.0.write(|db| {
+//         let highest = db.iter().max_by(|a, b| a.0.cmp(&b.0)).map(|(k, _v)| k).unwrap();
+//         let id = highest + 1;
 
-        let mut note = json_note.into_inner();
-        note.id = Some(id);
-        db.insert(id, note);
-    }).ok();
+//         let mut note = json_note.into_inner();
+//         note.id = Some(id);
+//         db.insert(id, note);
+//     }).ok();
 
-    db.0.save();
+//     db.0.save();
 
-    return json!({
-        "status": i32::from(404),
-        "msg": format!("note not found at id {}", id)
-    });
+//     return json!({
+//         "status": i32::from(404),
+//         "msg": format!("note not found at id {}", id)
+//     });
     
-}
+// }
 
-#[put("/note/<_id>", format = "json", data = "<json_note>")]
-fn update_note(_id: NoteID, json_note: Json<Note>) -> serde_json::Value {
-    let note = json_note.into_inner();
+// #[put("/note/<_id>", format = "json", data = "<json_note>")]
+// fn update_note(_id: NoteID, json_note: Json<Note>) -> serde_json::Value {
+//     let note = json_note.into_inner();
     
 
-    // find the note in the db by id (the id variable) and update it
-    // with the content of the PUTed note
+//     // find the note in the db by id (the id variable) and update it
+//     // with the content of the PUTed note
 
-    println!("{:#?}", note);
+//     println!("{:#?}", note);
 
-    json!({
-        "status": i32::from(200),
-        "msg": "note updated",
-        "data": {
-            "id": note.id().unwrap(),
-            "title": note.title(),
-            "content": note.content()
-        }
-    })
-}
+//     json!({
+//         "status": i32::from(200),
+//         "msg": "note updated",
+//         "data": {
+//             "id": note.id().unwrap(),
+//             "title": note.title(),
+//             "content": note.content()
+//         }
+//     })
+// }
 
-#[delete("/note/<_id>")]
-fn delete_note(_id: NoteID) -> serde_json::Value {
-    // Delete the note from the DB
-    json!({
-        "status": i32::from(200),
-        "msg": "note deleted",
-        "data": {}
-    })
-}
+// #[delete("/note/<_id>")]
+// fn delete_note(_id: NoteID) -> serde_json::Value {
+//     // Delete the note from the DB
+//     json!({
+//         "status": i32::from(200),
+//         "msg": "note deleted",
+//         "data": {}
+//     })
+// }
 
 
 
 #[launch]
 fn launch() -> _ {
-    let db = db::Database::connect("./enotes.db")
-        .expect("Could not connect to database");
-
     rocket::build()
         .register("/api/0.1.0", catchers![not_found])
-        .manage(db)
-        .mount("/api/0.1.0", routes![index, get_note, create_note, update_note, delete_note])
+        // .mount("/api/0.1.0", routes![index, get_note, create_note, update_note, delete_note])
+        .mount("/api/0.1.0", routes![index])
+
 }
